63fc20684e9ee6b800c174c8e329630f1d80431f
udayreddy4074 <201fa04074@gmail.com>
2025-01-29 13:01:17 +0530

    Add files via upload

---
+++ day3accspecifiers/Cust.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproDay3Demo
+{
+    //public access modifier
+    public class Customer
+    {
+        public int custId;
+        public string custName;
+
+        public void Display()
+        {
+            Console.WriteLine(" public  access specifier method");
+            Console.WriteLine($"CustomerId:{custId} \n CustomerName: {custName}");
+        }
+    }
+}
---
+++ day3accspecifiers/Prod.cs
@@ -0,0 +1,33 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproDay3Demo
+{
+   class Product
+    {
+       int Proid; //class members default it will be private
+       string ProName; //class members
+
+        public Product(int id,string name)
+        {
+            Proid = id;
+            ProName = name;
+        }
+
+        //define a method
+        private void SampleMthd()
+        {
+            Console.WriteLine("Private Method");
+        }
+        
+        public void PublicMthd()
+        {
+            Console.WriteLine("by prive accsee specifier");
+            Console.WriteLine("Public Method"+Proid+" "+ProName); 
+        }
+    }
+}
