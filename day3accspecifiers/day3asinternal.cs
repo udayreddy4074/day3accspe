2d43962f16a566f91709976a5cd3fb79a7a449a9
udayreddy4074 <201fa04074@gmail.com>
2025-01-29 13:43:13 +0530

    Add files via upload

---
+++ day3accspecifiers/day3asinternal.cs
@@ -0,0 +1,16 @@
+﻿using WiproDay3Demo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("internal acess specifier");
+        Customer cs = new Customer();
+        cs.custId = 1;
+        cs.custName = "UDAY ";
+        Console.WriteLine("using internal access specifier");
+        cs.Display();
+
+
+    }
+}
